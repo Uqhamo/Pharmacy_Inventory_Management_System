2593c8d31e9ce068c44a2985096e810eb4e76f54
Uqhamo <umekana05@gmail.com>
2026-09-20 16:17:06 +0200

    "initial commit"

--- src/pharmacy_inventory_management_system/AdminDashboard.java
+++ src/pharmacy_inventory_management_system/AdminDashboard.java
@@ -19,6 +19,32 @@ public class AdminDashboard extends JFrame {
      setLayout(new BorderLayout(10,10));
      JLabel label = new JLabel("Welcome to the Admin Dashboard",SwingConstants.CENTER);
  label.setFont(new Font("Arial",Font.BOLD,20));
- add(label);
+ add(label,BorderLayout.NORTH);
+ 
+ JPanel panelButtons  = new JPanel(new GridLayout(4,1,10,10));
+ panelButtons.setBorder(BorderFactory.createEmptyBorder(20,60,20,60));
+ 
+ 
+ JButton btnMedicine = new JButton("Medicine Management");
+ btnMedicine.addActionListener(e-> new MedicineManagement().setVisible(true));
+ 
+ 
+ JButton btnSupplier = new JButton("Suppiler Management");
+ btnSupplier.addActionListener(e-> new SupplierManagement().setVisible(true));
+ 
+ JButton btnUsers = new JButton("User Management");
+ btnUsers.addActionListener(e-> new MedicineManagement().setVisible(true));
+ 
+ JButton btnReports = new JButton("Reports");
+ btnReports.addActionListener(e-> JOptionPane.showMessageDialog(this,"Report Generation" ));
+   
+ panelButtons.add(btnMedicine);
+  panelButtons.add(btnSupplier);
+ panelButtons.add(btnUsers);
+ panelButtons.add(btnReports);
+ 
+ add(panelButtons, BorderLayout.CENTER);
+
+    
     }
 }
